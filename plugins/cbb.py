82f7d5a1f386d1ccec05a13707e676627756e230
botasupan <87362466+botasupan@users.noreply.github.com>
2021-09-18 00:13:14 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>✰ Owner : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n✰ Support : <a href='https://t.me/bokepviralindonesia_terbaru'>Channel</a>\n\n✰ Created by :<a href='https://t.me/nicogabisadesah'>Niko</a>",
+            text = f"<b>✰ Owner : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n✰ Support : <a href='https://t.me/bokepviralindonesia_terbaru'>Channel</a>\n\n✰ Modified by :<a href='https://t.me/nicogabisadesah'>NIKO</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
